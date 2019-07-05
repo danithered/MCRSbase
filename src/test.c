630b820a3cb7585441c58f7c7113b58381ad8cdc
danithered <danithered@live.com>
2019-07-05 18:35:31 +0200

    started to syncronize test.c

---
+++ src/test.c
@@ -0,0 +1,34 @@
+#include <mainheader.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <randomgen.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) { 
+	char *token, *read2, from[50] = "\0", read[256]="\0", argv2[256];
+	FILE *file;
+	int i;
+	
+	strcpy(from, argv[1]);
+	file = fopen(from, "r");
+	fgets(read, 256, file);
+	fgets(read, 256, file);
+	printf("\n%s\n", read);
+	
+	
+	//argv2 = (char *) calloc(10, sizeof(char[256]));
+	read2 = strdup(read);
+	for(i=1; ((token = strsep(&read2, " "))); i++ ) strcpy(argv[i], token);
+	
+	
+	
+	
+	for(i=1; i< argc; i++){
+		printf("\n%s", argv[i]);
+	}
+	
+	//free(argv2);
+	return(0);
+}
